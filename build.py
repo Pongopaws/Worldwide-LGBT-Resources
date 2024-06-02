b00c220c784a4aa33326b822baa4ab63e8fca526
Add Gaming <88384176+AddGaming@users.noreply.github.com>
2024-06-02 23:13:52 +0200

    transfered cirsis hotlines

--- build.py
+++ build.py
@@ -4,15 +4,15 @@ def main():
     home = os.getcwd()
     footer = ""
     header = ""
-    with open(f"{home}{os.sep}templates{os.sep}footer.html") as foot:
+    with open(f"{home}{os.sep}templates{os.sep}footer.html", encoding="utf8") as foot:
         footer = foot.read()
-    with open(f"{home}{os.sep}templates{os.sep}header.html") as head:
+    with open(f"{home}{os.sep}templates{os.sep}header.html", encoding="utf8") as head:
         header = head.read()
     for elem in os.listdir(f"{home}{os.sep}pages"):
         content = ""
-        with open(f"{home}{os.sep}pages{os.sep}{elem}") as con:
+        with open(f"{home}{os.sep}pages{os.sep}{elem}", encoding="utf8") as con:
             content = con.read()
-        with open(f"{home}{os.sep}{elem}", "w+") as f:
+        with open(f"{home}{os.sep}{elem}", "w+", encoding="utf8") as f:
             f.write(header + content + footer)
 
 if __name__ == "__main__":

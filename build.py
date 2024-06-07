b6e3b3a85be7de456dce9d1918ec376df857ed55
Add Gaming <88384176+AddGaming@users.noreply.github.com>
2024-06-07 15:13:15 +0200

    worked on social transition & set meta data

--- build.py
+++ build.py
@@ -1,5 +1,13 @@
 import os 
 
+def replace_title(original_header: str, file_name: str) -> str:
+    return original_header.replace(
+        "Wordwide Trans Resources", 
+        " ".join(
+            e.capitalize() for e in file_name[:-5].split("_")
+        )
+    )
+
 def main():
     home = os.getcwd()
     footer = ""
@@ -9,11 +17,12 @@ def main():
     with open(f"{home}{os.sep}templates{os.sep}header.html", encoding="utf8") as head:
         header = head.read()
     for elem in os.listdir(f"{home}{os.sep}pages"):
+        new_header = header if elem == "index.html" else replace_title(header, elem)
         content = ""
         with open(f"{home}{os.sep}pages{os.sep}{elem}", encoding="utf8") as con:
             content = con.read()
         with open(f"{home}{os.sep}{elem}", "w+", encoding="utf8") as f:
-            f.write(header + content + footer)
+            f.write(new_header + content + footer)
 
 if __name__ == "__main__":
     main()
